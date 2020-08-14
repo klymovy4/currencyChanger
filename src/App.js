7ea47c106a2ac6f21ae5b25be780ad6501afbde7
roman.klymovych <roman.klymovych@avenga.com>
2020-08-14 10:49:01 +0300

    Coctail done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Cocktails } from './pages/Cocktails';
+import { Profile } from './pages/Profile';
+
 
 function App() {
+
+  const [data, setData] = useState([
+    { id: Date.now(), name: 'John' },
+    { id: Date.now() + 1, name: 'Paul' },
+  ])
+  const [value, setValue] = useState('')
+  const changeHandlerValue = e => {
+    setValue(e.target.value)
+  }
+  const onClickHandler = (e) => {
+    e.preventDefault()
+    setData([
+      ...data, { id: Date.now(), name: value }
+
+    ])
+    setValue('')
+  }
+  // console.log('1', data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      {/*<div className='container pt-4'>*/}
+        <Switch>
+          <Route path='/' exact>
+            <Home data={data}
+              value={value}
+              onClickHAndler={onClickHandler}
+              changeHandlerValue={changeHandlerValue}
+            />
+          </Route>
+
+          <Route path='/cocktails' component={Cocktails} />
+          <Route path='/profile/:name' component={Profile} />
+        </Switch>
+
+
+
+      {/*</div>*/}
+    </BrowserRouter>
   );
 }
 
