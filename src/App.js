7dec687ea3cf5ef74c05e90bbffc7caa9671c778
roman.klymovych <roman.klymovych@avenga.com>
2020-08-21 19:03:12 +0300

    Started working on registerForm

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { Cocktails } from './pages/Cocktails';
-import { Profile } from './pages/Profile';
+import { Currency } from './pages/Currency';
+import { Registration } from './components/Registration'
 
 function App() {
   return (
         <BrowserRouter>
           <Navbar />
+            <Route path='/' exact>
+              <Registration />
+            </Route>
             <Switch>
-              <Route path='/' exact>
+              <Route path='/home'>
                 <Home />
               </Route>
 
-              <Route path='/cocktails' component={Cocktails} />
-              <Route path='/currency' component={Profile} />
+              <Route path='/cocktails'>
+                <Cocktails />
+              </Route>
+
+              <Route path='/currency'>
+                <Currency />
+              </Route>
             </Switch>
 
         </BrowserRouter>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => (
     <nav className='navbar navbar-expand bg-primary navbar-expand-lg'>
-        <div className='navbar-brand'>
-            Githab Search
-        </div>
+        <NavLink to='/' className='text-dark navbar-brand'>
+            Test Fetch
+        </NavLink>
         <ul className='navbar-nav'>
             <li className='nav-item '>
-                <NavLink exact to='/' className='nav-link text-white'>Main</NavLink>
+                <NavLink exact to='/home' className='nav-link text-white'>Main</NavLink>
             </li>
             <li className='nav-item'>
                 <NavLink to='/cocktails' className='nav-link text-white'>Cocktails</NavLink>
